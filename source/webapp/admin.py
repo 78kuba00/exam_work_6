082dde79154e3921cb674e05254c8852289290e2
Zhantorouulu_Kubat <78kuba00@gmail.com>
2022-11-12 12:32:18 +0600

    Step 1: Created and registered models.py in admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from webapp.models import Comment
 
-# Register your models here.
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'email', 'message', 'created_at', 'updated_at']
+    list_display_links = ['author']
+    list_filter = ['created_at']
+    search_fields = ['author', 'message']
+    exclude = []
+    readonly_fields = ['created_at', 'updated_at']
+
+admin.site.register(Comment, CommentAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model):
+    author = models.CharField(max_length=40, null=False, blank=False, default='-', verbose_name='Автор')
+    email = models.EmailField(max_length=50, null=False, blank=False, verbose_name='Почта')
+    message = models.TextField(max_length=1000,  null=False, blank=False, verbose_name='Запись')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+
+    def __str__(self):
+        return f'{self.pk}. {self.author}'
