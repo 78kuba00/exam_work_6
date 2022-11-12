130f08b0996c7a4cc007ae73843dbd4d9af3a3a0
Zhantorouulu_Kubat <78kuba00@gmail.com>
2022-11-12 13:29:41 +0600

    Step 2: added STATUS_CHOICES, fixed index_view to only show active comments

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+STATUS_CHOICES = [('active', 'Активно'), ('blocked', 'Заблокировано')]
+
 class Comment(models.Model):
     author = models.CharField(max_length=40, null=False, blank=False, default='-', verbose_name='Автор')
     email = models.EmailField(max_length=50, null=False, blank=False, verbose_name='Почта')
     message = models.TextField(max_length=1000,  null=False, blank=False, verbose_name='Запись')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    status = models.CharField(max_length=7, choices=STATUS_CHOICES, default='active', verbose_name="Статус")
 
     def __str__(self):
         return f'{self.pk}. {self.author}'
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from webapp.models import Comment
+
+def index_view(request, *args, **kwargs):
+    comments = Comment.objects.order_by('-created_at').filter(status='active')
+    context = {
+        'comments': comments
+    }
+    return render(request, "index.html", context)
 
 # Create your views here.
